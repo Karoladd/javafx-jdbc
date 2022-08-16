e871ab905dbb26332e85544cb75afd702a2a2e12
Karoladd <karoline859@hotmail.com>
2022-08-16 15:34:11 -0300

    Observer design pattern to update tableview

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -11,7 +11,7 @@ public class Utils {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
 	
-	public static Integer tryParseToInt(String str) {
+	public static Integer tryParseToInt (String str) {
 		try {
 			return Integer.parseInt(str);
 		}
